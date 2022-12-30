f69bc4380b7644492c503e6d302d6df9464b26c7
SarateanuC <cojocaru.carolina@mail.globaldatabase.com>
2022-12-30 15:48:22 +0200

    importByScript: add test-script

--- src/main/java/com/example/csvtodatabase/CsvToDatabaseApplication.java
+++ src/main/java/com/example/csvtodatabase/CsvToDatabaseApplication.java
@@ -1,13 +1,18 @@
 package com.example.csvtodatabase;
 
+import com.example.csvtodatabase.service.CsvService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class CsvToDatabaseApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CsvToDatabaseApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(CsvToDatabaseApplication.class, args);
+//        CsvService bean = run.getBean(CsvService.class);
+//        bean.csvToEntity("/home/administrator/Downloads/book/name_gender.csv");
+
     }
 
 }
---
+++ src/main/java/com/example/csvtodatabase/service/CsvService.java
@@ -0,0 +1,24 @@
+package com.example.csvtodatabase.service;
+
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CsvService {
+    @SneakyThrows
+    public void csvToEntity(String path) {
+        // List<User> users = new ArrayList<>();
+
+//        try (LineIterator lineIterator = lineIterator(new File(path), "UTF-8")) {
+//            while (lineIterator.hasNext()) {
+//                lineIterator.nextLine().;
+//            }
+//        try (CSVReader csvReader = new CSVReader(new FileReader(new File(path)))) {
+//            List<String[]> r = csvReader.readAll();
+//            csvReader.readAll().stream().map(e->e.)
+//            r.forEach(x -> System.out.println(Arrays.toString(x)));
+//        }
+    }
+
+}
+
