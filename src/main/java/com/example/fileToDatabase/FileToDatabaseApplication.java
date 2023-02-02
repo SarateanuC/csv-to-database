b96cf01ade8360b4117125bf14228fd4bc986645
SarateanuC <cojocaru.carolina@mail.globaldatabase.com>
2023-02-02 15:54:21 +0200

    importByScript: add hibernate batch processing

--- src/main/java/com/example/fileToDatabase/FileToDatabaseApplication.java
+++ src/main/java/com/example/fileToDatabase/FileToDatabaseApplication.java
@@ -1,17 +1,13 @@
 package com.example.fileToDatabase;
 
-import com.example.fileToDatabase.service.impl.JsonService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class FileToDatabaseApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext run = SpringApplication.run(FileToDatabaseApplication.class, args);
-        JsonService bean = run.getBean(JsonService.class);
-        bean.testJson("/home/administrator/Downloads/book/20190902_1.json");
+        SpringApplication.run(FileToDatabaseApplication.class, args);
     }
 
 }
